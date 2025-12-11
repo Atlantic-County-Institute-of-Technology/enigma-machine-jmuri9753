68f9219417e4928ce3eff6dc1b8840bdde89e7f2
jmuri9753 <muri9753@student.acitech.org>
2025-12-11 10:52:51 -0500

    got the key to work with gavins help

--- main.py
+++ main.py
@@ -3,43 +3,49 @@
 # Last Edit: 12.9.25
 
 import os
-import inquirer3
+# import inquirer3
 
 UPPER_CASE = 65
 LOWER_CASE = 97
 ALPHABET_SIZE = 26
+phrase = str(input("[-] Please Input A Phrase: "))
+key = str("Banana")
 
-# def get_key_index(key):
-#     for i in key:
-#         if i.isalpha():
-#             charset = (UPPER_CASE if i.isupper() else LOWER_CASE)
-#             key_index = ord(i) - charset
-#     return key_index
+def get_key_index(key,phrase):
+    if range(len(key)) == range(len(phrase)):
+        list_indexes = []
+        for i in key:
+            if i.isalpha():
+                charset = (UPPER_CASE if i.isupper() else LOWER_CASE)
+                indexes = (ord(i) - charset)
+                list_indexes.append(indexes)
+        return list_indexes
+
+    else:
+        pass
+
+key_index = get_key_index(key,phrase)
 
 
-def vigenere_cipher(phrase, key):
+def vigenere_cipher(phrase, key_index):
     encoded_phrase = ''
-    for letter in phrase:
-        if letter.isalpha():
-            charset = (UPPER_CASE if letter.isupper() else LOWER_CASE)
-            for i in key:
-                key_index = (ord(i) - charset)
-                print(key_index)
+    for letter in range(len(phrase)):
+        if phrase[letter].isalpha():
+            charset = (UPPER_CASE if phrase[letter].isupper() else LOWER_CASE)
 
             
-            encoded_phrase += chr((ord(letter) - charset + key_index) % ALPHABET_SIZE + charset)
-            print(encoded_phrase)
+            encoded_phrase += chr((ord(phrase[letter]) - charset + key_index[letter]) % ALPHABET_SIZE + charset)
             
         else:
             encoded_phrase += letter
 
+    return encoded_phrase
+
 
 
 def main():
-    phrase = str(input("[-] Please Input A Phrase: "))
-    key = str("banana")
-    vigenere_cipher(phrase,key)
-
+    encoded_phrase = vigenere_cipher(phrase,key_index)
+    print(encoded_phrase)
 
 
 if __name__ == "__main__":
