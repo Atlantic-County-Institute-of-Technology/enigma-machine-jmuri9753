304c632640fe16968aa2b28dba05df641e224823
jmuri9753 <muri9753@student.acitech.org>
2025-12-12 10:54:33 -0500

    code does not work.... only for lower case though for some weird reason

--- main.py
+++ main.py
@@ -9,32 +9,36 @@ UPPER_CASE = 65
 LOWER_CASE = 97
 ALPHABET_SIZE = 26
 phrase = str(input("[-] Please Input A Phrase: "))
-key = str("Banana")
+key = "banana"
 
 def get_key_index(key,phrase):
     if range(len(key)) == range(len(phrase)):
         list_indexes = []
         for i in key:
             if i.isalpha():
-                charset = (UPPER_CASE if i.isupper() else LOWER_CASE)
-                indexes = (ord(i) - charset)
-                list_indexes.append(indexes)
+                # charset = (UPPER_CASE if i.isupper() else LOWER_CASE)
+                # indexes = (ord(i) - charset)
+                list_indexes.append(i)
         return list_indexes
 
     else:
-        pass
+        for i in range(len(phrase) - len(key)):
+            key_add = (key[i % len(key)])
+            key += key_add
+        return "".join(key)
 
 key_index = get_key_index(key,phrase)
-
+# print(key_index)
 
 def vigenere_cipher(phrase, key_index):
     encoded_phrase = ''
     for letter in range(len(phrase)):
         if phrase[letter].isalpha():
             charset = (UPPER_CASE if phrase[letter].isupper() else LOWER_CASE)
-
             
-            encoded_phrase += chr((ord(phrase[letter]) - charset + key_index[letter]) % ALPHABET_SIZE + charset)
+            # print((ord(phrase[letter]) - charset + (ord(key_index[letter])) - charset) % ALPHABET_SIZE + charset)
+            
+            encoded_phrase += chr((ord(phrase[letter]) - charset + (ord(key_index[letter])) - charset) % ALPHABET_SIZE + charset)
             
         else:
             encoded_phrase += letter
