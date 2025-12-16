7169d9f80a6f582bda353d87b05ec95422d801e2
jmuri9753 <muri9753@student.acitech.org>
2025-12-16 10:52:20 -0500

    I did my cipher without space but now its buggin out with spaces

--- main.py
+++ main.py
@@ -16,45 +16,62 @@ def get_key_index(key,phrase):
         list_indexes = []
         for i in key:
             if i.isalpha():
-                # charset = (UPPER_CASE if i.isupper() else LOWER_CASE)
-                # indexes = (ord(i) - charset)
                 list_indexes.append(i)
         return list_indexes
 
     else:
         for i in range(len(phrase) - len(key)):
+            print(i)
+            if phrase[i].isalpha():
+                print("yes")
+            else:
+                print("no")
             key_add = (key[i % len(key)])
             key += key_add
         return "".join(key)
 
 key_index = get_key_index(key,phrase)
-# print(key_index)
 
-def vigenere_cipher(phrase, key_index):
+def vigenere_cipher_encode(phrase, key_index):
     encoded_phrase = ''
     for letter in range(len(phrase)):
+        print(f"{phrase[letter], key_index[letter]} \n")
         if phrase[letter].isalpha():
-            print(phrase[letter].lower())
             phrase_lower = phrase[letter].lower()
-            charset = (UPPER_CASE if phrase[letter].isupper() else LOWER_CASE)
-            
-            # print((ord(phrase[letter]) - charset + (ord(key_index[letter])) - charset) % ALPHABET_SIZE + charset)
-            print(f"Ordinal of phrase letter: {ord(phrase_lower)}\n"
-                  f"Ordinal value of key letter: {ord(key_index[letter])}\n"
-                  f"Shift value: {ord(key_index[letter]) - charset}\n"
-                  f"Result value: {(ord(phrase[letter]) - charset + (ord(key_index[letter])) - charset) % (ALPHABET_SIZE + charset)}")
-            encoded_phrase += chr((ord(phrase_lower) - charset + (ord(key_index[letter])) - charset) % (ALPHABET_SIZE + charset))
-            
+            charset = (UPPER_CASE if phrase_lower.isupper() else LOWER_CASE)
+
+            if phrase[letter] == phrase_lower:
+                encoded_phrase += chr((ord(phrase_lower) - charset + (ord(key_index[letter]) - charset)) % ALPHABET_SIZE + charset)
+            else:
+                encoded_phrase += chr((ord(phrase_lower) - charset + (ord(key_index[letter]) - charset)) % ALPHABET_SIZE + charset).upper()
         else:
-            encoded_phrase += letter
+            encoded_phrase += phrase[letter]
 
     return encoded_phrase
 
 
+def vigenere_cipher_decode(encoded_phrase, key_index):
+    decoded_phrase = ''
+    for letter in range(len(encoded_phrase)):
+        if phrase[letter].isalpha():
+            phrase_lower = encoded_phrase[letter].lower()
+            charset = (UPPER_CASE if phrase_lower.isupper() else LOWER_CASE)
+
+            if encoded_phrase[letter] == phrase_lower:
+                decoded_phrase += chr((ord(phrase_lower) - charset - (ord(key_index[letter]) - charset)) % ALPHABET_SIZE + charset)
+            else:
+                decoded_phrase += chr((ord(phrase_lower) - charset - (ord(key_index[letter]) - charset)) % ALPHABET_SIZE + charset).upper()
+        else:
+            decoded_phrase += encoded_phrase[letter]
+
+    return decoded_phrase
 
 def main():
-    encoded_phrase = vigenere_cipher(phrase,key_index)
+    get_key_index(phrase, key)
+    encoded_phrase = vigenere_cipher_encode(phrase,key_index)
+    decoded_phrase = vigenere_cipher_decode(encoded_phrase, key_index)
     print(encoded_phrase)
+    print(decoded_phrase)
 
 
 if __name__ == "__main__":
